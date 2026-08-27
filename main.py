e6084d3be293f01471aa7e1fd1c29e3ea890fdd2
Nikhilteja-deft <Nikhil.C@deftsquad.com>
2026-08-27 11:52:53 -0500

    fix: when to use async or normal function

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+
+class Items(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 @app.get("/")
@@ -10,4 +23,15 @@ async def root():
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    data = Item(name=name, price=100, is_offer=True)
+    return {"message": f"Hello {data.name} your total is {data.price} "}
+
+
+@app.post("/items/")
+async def create_item(item: Items):
+    response = item | {"id": 1}
+    return response
+
+@app.get("/home/{item_id}")
+def read_item(item_id: int):
+    return f"Welcome to the home page {item_id}"
