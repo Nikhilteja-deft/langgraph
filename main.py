c7d839aea954af74d656fd0acd8cd0e94cc213ca
Nikhilteja-deft <Nikhil.C@deftsquad.com>
2026-08-31 07:32:06 -0500

    feat: add endpoint for comparing and reviewing protocols

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import fastapi_jsonrpc as jsonrpc
 from fastapi import Body
 from pydantic import BaseModel
+from agent.simple import graph
+from uuid import uuid4
+from langgraph.types import Command
 
 
 # so the json rpc is built on top of the fast api
@@ -8,6 +11,16 @@ app = jsonrpc.API()
 # so all the json rpc calls come to this end point
 rpc = jsonrpc.Entrypoint("/rpc")
 
+
+class ProtocolRequest(BaseModel):
+    protocol_id: str
+    old_version: str
+    new_version: str
+
+class ReviewRequest(BaseModel):
+    thread_id: str
+    action: str
+    comment: str | None = None
 
 class Item(BaseModel):
     name: str
@@ -44,6 +57,57 @@ async def create_item(item: Items):
 def read_item(item_id: int):
     return f"Welcome to the home page {item_id}"
 
+
+
+
+@app.post("/api/v1/protocol/compare")
+def compare_protocol(request: ProtocolRequest):
+
+    thread_id = str(uuid4())
+
+    config = {
+        "configurable": {
+            "thread_id": thread_id
+        }
+    }
+
+    response = graph.invoke(
+        {
+            "protocol_id": request.protocol_id,
+            "old_version": request.old_version,
+            "new_version": request.new_version
+        },
+        config=config
+    )
+
+    return {
+        "thread_id": thread_id,
+        "result": response
+    }
+
+
+
+
+@app.post("/api/v1/protocol/review")
+def review_protocol(request: ReviewRequest):
+
+    config = {
+        "configurable": {
+            "thread_id": request.thread_id
+        }
+    }
+
+    response = graph.invoke(
+        Command(
+            resume={
+                "action": request.action,
+                "comment": request.comment
+            }
+        ),
+        config=config
+    )
+
+    return response
 
 # JSON-RPC method
 @rpc.method()
